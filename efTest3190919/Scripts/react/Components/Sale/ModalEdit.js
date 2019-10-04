765133713dbaf1ba56cfe999a1cb8d737007c1f2
Cédric David <cedric.david1974@gmail.com>
2019-10-04 12:30:35 +1000

    resolved an error in sales editmodal

--- efTest3190919/Scripts/react/Components/Sale/ModalEdit.js
+++ efTest3190919/Scripts/react/Components/Sale/ModalEdit.js
@@ -55,7 +55,6 @@ class ModalEdit extends React.Component {
             return { modalOpen: true };
         });
     }
-
     editClickHandler(e) {
         e.preventDefault;
         if (!(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/g).test(this.state.dateSoldSelected)) {
